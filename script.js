9e13ecad361d15c7753522d5d01cca8fe2e9d386
NihanShahriarPalock <nihanshahriar000@gmail.com>
2024-02-14 01:04:27 +0600

    Rectangle  Done

--- script.js
+++ script.js
@@ -19,4 +19,28 @@ function calculateTriangleArea() {
     spanTriangleArea.innerText = triangleArea;
     
 }
+//  Calculate Rectangle Area
+function calculateRectangleArea() {
+    const rectangleBase = getInputValue('rectangleBase');
+    const rectangleHeight = getInputValue('rectangleHeight')
+    const rectangleArea = 0.5 * rectangleBase * rectangleHeight;
+    console.log(rectangleArea);
+
+    //  Display Value in span tag
+    const spanRectangleArea = document.getElementById('rectangleSetArea');
+    spanRectangleArea.innerText = rectangleArea;
+
+}
+
+function calculateEllipseArea() {
+    const ellipseRadius = getInputValue('ellipseRadius');
+
+    const ellipseArea = 3.1416 * ellipseRadius * ellipseRadius;
+    console.log(ellipseArea);
+
+    //  Display Value in span tag
+    const spanEllipseArea = document.getElementById('ellipseSetArea');
+    spanEllipseArea.innerText = ellipseArea;
+
+}
 
