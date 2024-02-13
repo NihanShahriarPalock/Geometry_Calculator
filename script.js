8703b730682b7867b7aafe180b6f71cecaf1ba5f
NihanShahriarPalock <nihanshahriar000@gmail.com>
2024-02-14 00:35:05 +0600

    Triangle Done

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-{\rtf1}
+function getInputValue(inputValueText) {
+    const inputText = document.getElementById(inputValueText);
+    const inputValueInt = inputText.value;
+    const inputValue = parseFloat(inputValueInt);
+    return inputValue;
+
+    
+}
+
+// Calculate Triangle Area
+function calculateTriangleArea() {
+    const triangleBase = getInputValue('triangleBase');
+    const triangleHeight = getInputValue('triangleHeight')
+    const triangleArea = 0.5 * triangleBase * triangleHeight;
+    console.log(triangleArea);
+   
+    //  Display Value in span tag
+    const spanTriangleArea = document.getElementById('triangleSetArea');
+    spanTriangleArea.innerText = triangleArea;
+    
+}
+
